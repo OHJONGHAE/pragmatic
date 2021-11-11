eb2af4b9b730db706ccf6894030bba98d31d02b6
애플 <ojh@aepeul-ui-MacBook-Pro.local>
2021-11-11 15:36:17 +0900

    djust Django course 32 commit

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,5 +1,39 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, UpdateView
+
+from profileapp.decorators import profile_ownership_required
+from profileapp.forms import ProfileCreationForm
+from profileapp.models import Profile
 
 
+class ProfileCreateView(CreateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/create.html'
+
+    def form_valid(self, form):
+        '''
+        form : ProfileCreateForm 에서 날라온 데이터
+        commit=False : 실제 DB에 저장하지 않고 임시적 데이터
+        .save : DB에 실제 반영시켜주는 역
+        '''
+        temp_profile = form.save(commit=False)
+        temp_profile.user = self.request.user
+        temp_profile.save()
+
+        return super().form_valid(form)
+
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/update.html'
